7d02a31533c4241ef5e429bec273b92ae6d54542
Bhanu <sunkabhanu281202@gmail.com>
2021-06-30 19:29:31 +0530

    Reverse_Function_Factorial

---
+++ Clg Stuff/exp_13.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact(int x)
+{
+	int i,m=1;
+	for(i=1;i<=x;i++)
+	{
+		m=m*i;
+	}
+	return m;
+}
+int main()
+{
+	int n,e,a;
+	printf("Enter a number=");
+	scanf("%d",&n);
+	e=fact(n);
+	printf("\nFactorial=%d",e);
+	return 0;
+}
