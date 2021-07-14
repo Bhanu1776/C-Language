e62e6d8af42204e08813afdbcbfedbd6ea49cc3f
Bhanu <sunkabhanu281202@gmail.com>
2021-07-14 14:11:22 +0530

    IMP

---
+++ Chap_5/0_Types of Functions.c
@@ -0,0 +1,62 @@
+//  Functions With No Arguments And No Return Value (4th)
+
+#include<stdio.h>
+
+void mahesh();
+
+int main()
+{
+    printf("Myself Mahesh Rathod\n");
+    mahesh();
+    printf("Lives in Dombivli\n");
+    return 0;
+}
+
+void mahesh(){
+    printf("I'm 19yrs old\n");
+} 
+------------------------------------------------------
+
+// Functions With no Argument but a return value (2nd)
+
+#include<stdio.h>
+
+int mahesh(void);
+
+int main()
+{
+    int a;
+    printf("Mahesh\n");
+    a=mahesh();
+    return 0;
+}
+
+int mahesh (void){
+    int b;
+    b = printf("Rathod");
+    return b;
+}
+------------------------------------------------------
+
+// Functions with Argument and no return value (3rd)  Refer pgno 137 from notes !!
+
+------------------------------------------------------
+
+// Functions with Argument and return value (1st)
+
+#include<stdio.h>
+
+int sum (int x, int y);
+
+int main()
+{
+    int a=7,b=3;
+    printf("The sum of value a = %d and be = %d is %d", a,b,sum(a,b));
+    return 0;
+}
+
+int sum (int x, int y){
+    int s;
+    s=x+y;
+    return s;
+}
