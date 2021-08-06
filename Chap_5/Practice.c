d6a8c21d28dc909be17bf0576348ef740d91779e
Bhanu <sunkabhanu281202@gmail.com>
2021-08-06 20:02:51 +0530

    Even_odd

---
+++ Chap_5/Practice.c
@@ -0,0 +1,35 @@
+/*#include<stdio.h>
+
+### EVEN ODD ###
+
+int output(int input);
+
+int main()
+{
+    int n,o;
+    printf("Enter the value = ");
+    scanf("%d",&n);
+
+    o = output(n);
+
+    return 0;
+}
+
+int output(int input){
+    int o,n;
+    if (n%2==0){
+        printf("Your Entered no. is EVEN ");
+    }
+    else{
+        printf("Your Entered no. is Odd ");
+    }
+    return (input);
+}*/
+
+#include<stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
