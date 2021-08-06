4a316c653796dc0eb45074eb88af2a38117b2854
Bhanu <sunkabhanu281202@gmail.com>
2021-08-06 20:45:05 +0530

    String_basic

---
+++ Chap_8/1.string_basic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// String == String is an array of characters ..
+
+int main()
+{
+    char N[5] = "Bhanu";  
+
+    for (int i=0;i<5;i++){
+        printf("%c",N[i]);
+    }
+    return 0;
+}
