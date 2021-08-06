a74445f2d0669b73f75d6ed4d4dd00f4b6b3f3d4
Bhanu <sunkabhanu281202@gmail.com>
2021-08-06 20:46:05 +0530

    print_Easy_way

---
+++ Chap_8/2.Easy_%s.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+                                // * Easiest way to print string contents ..
+int main()
+{
+    char *ptr = "Bhanu";
+    printf("%s\n",ptr); 
+
+    // OR \\
+
+    char s[10] = "Bhanu"; 
+    printf("%s",s);  
+
+    return 0;
+} 
