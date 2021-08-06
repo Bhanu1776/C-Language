fb1f16ade9ea83305163de75cbdeb5814126f0c9
Bhanu <sunkabhanu281202@gmail.com>
2021-08-06 20:50:14 +0530

    strcmp(compare)

---
+++ Chap_8/8.strcmp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main()
+{
+    char st1[20] = "Bhanu";
+    char *st2 = "Genius";
+    int value = strcmp(st1,st2);   // This func is used to compare two strings .. 
+
+    printf("Now the value of is %d",value);   // To know how ouput value occured, refer notes !!
+    return 0;
+} 
