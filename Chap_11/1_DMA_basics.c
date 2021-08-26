0634612a83238fda8cb427ab739538c59e1e3f9a
Bhanu <sunkabhanu281202@gmail.com>
2021-08-27 01:49:14 +0530

    Dynamic Memory Allocation

--- Chap_11/1_DMA_basics.c
+++ Chap_11/1_DMA_basics.c
@@ -3,7 +3,7 @@
 //* Dynamic Memory Allocation === It is a way to allocate memory to a data structures during a run time ..
 
 int main()
-{   // size of data tupes 
+{   // size of data tupes ..
 
     printf("The size of int in my pc is %d\n", sizeof(int));
     printf("The size of float in my pc is %d\n", sizeof(float));
